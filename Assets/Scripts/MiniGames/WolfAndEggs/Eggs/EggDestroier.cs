e53146f82e3abadc92f0fa2d5d891d097e33c032
Savelus <savelussava@mail.ru>
2023-03-30 14:29:48 +0500

    More DI and configs

--- Assets/Scripts/MiniGames/WolfAndEggs/Eggs/EggDestroier.cs
+++ Assets/Scripts/MiniGames/WolfAndEggs/Eggs/EggDestroier.cs
@@ -1,17 +1,7 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
-namespace MiniGames.WolfAndEggs.Eggs
+public class EggDestroier : MonoBehaviour
 {
-    public class EggDestroier : MonoBehaviour
-    {
-        /*
-        private void OnCollisionEnter2D(Collision2D collision)
-        {
-            if (collision.gameObject.GetComponent<Egg>())
-            {
-                Destroy(collision.gameObject);
-            }
-        }
-        */
-    }
 }
---
+++ Assets/Scripts/MiniGames/WolfAndEggs/Settings/SceneSettings.cs
@@ -0,0 +1,13 @@
+﻿using TMPro;
+using UnityEngine;
+using UnityEngine.Serialization;
+using UnityEngine.UI;
+
+namespace MiniGames.WolfAndEggs
+{
+    public class SceneSettings : MonoBehaviour
+    {
+        public Button switchStateButton;
+        public TMP_Text buttonText;
+    }
+}
